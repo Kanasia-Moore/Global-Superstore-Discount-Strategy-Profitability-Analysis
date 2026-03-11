dd5e2bfe27f0806cf7789aad06cc3c6a9c241cdc
Kanasia Moore <moorek526@gmail.com>
2026-03-11 11:57:34 -0400

    Update 02_kpi_definitions.sql

--- SQL/02_kpi_definitions.sql
+++ SQL/02_kpi_definitions.sql
@@ -1,12 +1,3 @@
--- ======================================================
--- KPI Base Table Creation
--- Purpose:
--- Create a metrics-ready dataset combining Orders,
--- Returns, and People tables with derived KPI fields.
--- This dataset will be used for all downstream analysis.
--- ======================================================
-
-
 -- Superstore base table view (line-level)
 CREATE VIEW superstore_base AS 
 SELECT 
@@ -50,89 +41,11 @@ SELECT
 FROM `superstore_base`
 GROUP BY Order_Id;
 
+-- Overall KPI
 Select 
   COUNT(*) AS Total_Orders,
   COUNTIF(Return_Flag = 1) AS Total_Returns,
   ROUND(COUNTIF(Return_Flag = 1) / COUNT(*) * 100, 1) AS Return_Rate
 FROM superstore_order_summary
-
--- Return Rate per Category
-SELECT
-  Category,
-  COUNT(DISTINCT Order_ID) AS Orders_by_Category,
-  COUNT(DISTINCT CASE WHEN Return_Flag = 1 THEN Order_ID END) AS Return_Orders,
-  ROUND(SUM(Sales), 2) Sales_by_Cat,
-  ROUND(SUM(Profit), 2) Profit_by_Cat,
-  ROUND(SAFE_DIVIDE(
-          COUNT(DISTINCT CASE WHEN Return_Flag = 1 THEN Order_ID END),
-          COUNT(DISTINCT Order_ID)) * 100, 2) AS Return_Rate_Percent
-FROM `superstore_base`
-GROUP BY Category
-ORDER BY Return_Rate_Percent DESC;
-
--- Return Rate Per Sub-Category
-SELECT
-  `Sub-Category`,
-  COUNT(DISTINCT Order_ID) AS Orders_by_SubCat,
-  COUNT(DISTINCT CASE WHEN Return_Flag = 1 THEN Order_ID END) AS Return_Orders,
-  ROUND(SUM(Sales), 2) Sales_by_SubCat,
-  ROUND(SUM(Profit), 2) Profit_by_SubCat,
-  ROUND(SAFE_DIVIDE(
-          COUNT(DISTINCT CASE WHEN Return_Flag = 1 THEN Order_ID END),
-          COUNT(DISTINCT Order_ID)) * 100, 2) AS Return_Rate_Percent
-FROM `superstore_base`
-GROUP BY `Sub-Category`
-ORDER BY Return_Rate_Percent DESC
-
--- Return Rate Per Region
-SELECT
-  Region,
-  COUNT(*) AS Orders_by_Region,
-  COUNTIF(Return_Flag = 1) AS Return_Orders,
-  ROUND(SUM(Sales), 2) Sales_by_Region,
-  ROUND(SUM(Profit), 2) Profit_by_Region,
-  ROUND(SAFE_DIVIDE(COUNTIF(Return_Flag = 1),COUNT(Order_ID)) * 100, 2) AS Return_Rate_Percent
-FROM `superstore_order_summary`
-GROUP BY Region
-ORDER BY Return_Rate_Percent DESC
-
--- Return Rate per Segment
-SELECT
-  Segment,
-  COUNT(*) AS Orders_by_Segment,
-  COUNTIF(Return_Flag = 1) AS Return_Orders,
-  ROUND(SUM(Sales), 2) Sales_by_Segment,
-  ROUND(SUM(Profit), 2) Profit_by_Segment,
-  ROUND(SAFE_DIVIDE(COUNTIF(Return_Flag = 1),COUNT(Order_ID)) * 100, 2) AS Return_Rate_Percent
-FROM `superstore_order_summary`
-GROUP BY Segment
-ORDER BY Return_Rate_Percent DESC
-
--- Return Rate per Discount Band
-SELECT
-  Discount_Band,
-  COUNT(DISTINCT Order_ID) AS Orders_per_Band,
-  COUNT(DISTINCT CASE WHEN Return_Flag = 1 THEN Order_ID END) AS Return_Orders,
-  ROUND(AVG(Line_Profit_Margin), 2) AS Avg_Profit_Margin,
-  ROUND(SAFE_DIVIDE(
-          COUNT(DISTINCT CASE WHEN Return_Flag = 1 THEN Order_ID END),
-          COUNT(DISTINCT Order_ID)) * 100, 2) AS Return_Rate_Percent
-FROM `superstore_base`
-GROUP BY Discount_Band
-ORDER BY Return_Rate_Percent DESC
-
--- Priortity Fix List
-SELECT
-  `Sub-Category`,
-  ROUND(SUM(Sales), 2) Sales_by_SubCat,
-  ROUND(SUM(Profit), 2) Profit_by_SubCat,
-  ROUND(SAFE_DIVIDE(SUM(Profit), NULLIF(SUM(Sales), 0)) * 100, 2) AS Profit_Margin,
-  ROUND(Avg(Discount) * 100, 2) AS Avg_Discount,
-  ROUND(SAFE_DIVIDE(
-          COUNT(DISTINCT CASE WHEN Return_Flag = 1 THEN Order_ID END),
-          COUNT(DISTINCT Order_ID)) * 100, 2) AS Return_Rate_Percent
-FROM `superstore_base`
-GROUP BY `Sub-Category` 
-ORDER BY Return_Rate_Percent DESC;
 
 
