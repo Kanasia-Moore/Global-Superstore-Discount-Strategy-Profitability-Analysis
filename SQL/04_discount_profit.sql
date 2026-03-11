b67d586d9e3d1a9e2f5d5987ed91d3c81c263af0
Kanasia Moore <moorek526@gmail.com>
2026-03-11 12:52:32 -0400

    Update 04_discount_profit.sql

--- SQL/04_discount_profit.sql
+++ SQL/04_discount_profit.sql
@@ -1,24 +1,3 @@
--- Profit by Category
-SELECT
-  Category,
-  ROUND(SUM(Sales), 2) AS Total_Sales,
-  ROUND(SUM(Profit), 2) AS Total_Profit,
-  ROUND(SAFE_DIVIDE(SUM(Profit), NULLIF(SUM(Sales), 0)) * 100, 2) AS Profit_Margin,
-  ROUND(AVG(Discount) * 100, 2) AS Avg_Discount_Percentage
-FROM `superstore_base`
-GROUP BY Category
-
--- Profit by Sub-category
-SELECT
-  `Sub-Category`,
-  ROUND(SUM(Sales), 2) AS Total_Sales,
-  ROUND(SUM(Profit), 2) AS Total_Profit,
-  ROUND(SAFE_DIVIDE(SUM(Profit), NULLIF(SUM(Sales), 0)) * 100, 2) AS Profit_Margin,
-  ROUND(AVG(Discount) * 100, 2) AS Avg_Discount_Percentage
-FROM `superstore_base`
-GROUP BY `Sub-Category`
-ORDER BY Profit_Margin DESC
-
 -- Profit Margin per Discount Band
 SELECT
   Discount_Band,
